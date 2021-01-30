92784f64bdfb566904f729af2933dedad24698a2
Sang-heon <sangheon@dayoff.ai>
2021-01-30 14:48:34 +0900

    Add config & body-parser

---
+++ config/key.js
@@ -0,0 +1,6 @@
+// NODE_ENV ==> production or development
+if(process.env.NODE_ENV === 'production') { 
+    module.exports = require('./prod')
+}else {
+    module.exports = require('./dev')
+}
--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 4000
+const { User } = require('./models/User')
+const bodyParser = require('body-parser')
+const config = require('./config/key')
+
+// application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+// application/json
+app.use(bodyParser.json());
+
 
 const mongoose = require('mongoose')
-
-mongoose.connect('mongodb+srv://sangheon:abcd1234@boiler-plate.v2rw8.mongodb.net/<dbname>?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then( ()=> console.log('MongDB Connected...'))
   .catch( err => console.log(err))
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! I\'m learn about Js')
+})
+
+app.post('/register', (req,res) => {
+    // 회원가입할 떄 입력한 정보들을 Client에서 가져오면
+    // 그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
